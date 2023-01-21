188285d46ef3ea7d70ae048304fc8c8f55c81310
misharyduk <misharyduk64@gmail.com>
2023-01-21 11:29:44 +0200

    created spring boot application entrypoint

--- src/main/java/com/fullstackapp/PublishingApp.java
+++ src/main/java/com/fullstackapp/PublishingApp.java
@@ -1,2 +1,11 @@
-package com.fullstackapp;public class PublishingApp {
+package com.fullstackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishingApp {
+  public static void main(String[] args) {
+    SpringApplication.run(PublishingApp.class, args);
+  }
 }
