c05af676ad088dfb94ba69e9cb6b6b9dd6b3106f
misharyduk <misharyduk64@gmail.com>
2023-02-08 18:29:28 +0200

    add RESTful controller related to Categories with crud operations endpoints

---
+++ category-service/src/main/java/com/publishing/category/CategoryController.java
@@ -0,0 +1,49 @@
+package com.publishing.category;
+
+import com.publishing.exception.CategoryException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+  private final CategoryService categoryService;
+
+  @GetMapping("{id}")
+  public Category getCategoryById(@PathVariable("id") Integer id) throws CategoryException {
+    // TODO: handle exception
+    return categoryService.getCategoryById(id);
+  }
+
+  @GetMapping
+  public List<Category> getAllCategories(){
+    return categoryService.getAllCategories();
+  }
+
+  @PostMapping
+  public Category saveCategory(@RequestBody CategoryRequest categoryRequest){
+    return categoryService.saveCategory(categoryRequest);
+  }
+
+  @PutMapping("{id}")
+  public Category updateCategory(@PathVariable("id") Integer id, @RequestBody CategoryRequest categoryRequest)
+      throws CategoryException {
+    // TODO: handle exception
+    return categoryService.updateCategory(id, categoryRequest);
+  }
+
+  @DeleteMapping("{id}")
+  public void deleteCategory(@PathVariable("id") Integer id){
+    categoryService.deleteCategoryById(id);
+  }
+}
