c9415321c35f5e8d6f7be42c0e3c817bdb24e9f7
misharyduk <misharyduk64@gmail.com>
2023-02-09 18:41:59 +0200

    add persistence annotations

--- clients-common/src/main/java/com/publishing/clients/user/User.java
+++ clients-common/src/main/java/com/publishing/clients/user/User.java
@@ -1,5 +1,6 @@
 package com.publishing.clients.user;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,11 +10,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class User {
+    @SequenceGenerator(
+            name = "app_user_sequence",
+            sequenceName = "app_user_sequence"
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "app_user_sequence"
+    )
     private Integer id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+    @Enumerated(value = EnumType.STRING)
     private Role role;
 }
