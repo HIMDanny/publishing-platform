bd16f643771594b0936fd9fc62418e946222f9bb
misharyduk <misharyduk64@gmail.com>
2023-02-09 17:26:51 +0200

    enable eureka client

--- article-service/src/main/java/com/publishing/ArticleApplication.java
+++ article-service/src/main/java/com/publishing/ArticleApplication.java
@@ -2,8 +2,10 @@ package com.publishing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ArticleApplication {
 
   public static void main(String[] args) {
