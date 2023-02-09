4c02296b56a3ef34cd8c31292a13cd759d6e3919
misharyduk <misharyduk64@gmail.com>
2023-02-09 14:28:07 +0200

    add open feign client related to user rest endpoint

---
+++ clients-common/src/main/java/com/publishing/clients/user/UserClient.java
@@ -0,0 +1,19 @@
+package com.publishing.clients.user;
+
+import com.publishing.clients.auth.RegisterRequest;
+import java.util.Optional;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient("user")
+public interface UserClient {
+
+    @PostMapping("api/v1/user")
+    boolean saveUser(@RequestBody RegisterRequest user);
+
+    @GetMapping("api/v1/user/{email}")
+    Optional<RegisterRequest> getByEmail(@PathVariable String email);
+}
