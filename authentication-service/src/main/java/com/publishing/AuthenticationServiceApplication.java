ea9e7df84de4f8cad3d4795bb5e2b7b02bef01de
misharyduk <misharyduk64@gmail.com>
2023-02-10 15:27:56 +0200

    remove unused import

--- authentication-service/src/main/java/com/publishing/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/publishing/AuthenticationServiceApplication.java
@@ -3,7 +3,6 @@ package com.publishing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
