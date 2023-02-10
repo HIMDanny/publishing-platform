62dcc167e089a12da5e12dd9ea303d0a43d0ba3c
misharyduk <misharyduk64@gmail.com>
2023-02-10 14:11:59 +0200

    enable feign clients for authentication service

--- authentication-service/src/main/java/com/publishing/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/publishing/AuthenticationServiceApplication.java
@@ -1,9 +1,17 @@
 package com.publishing;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@EnableFeignClients(
+        basePackages = "com.publishing.clients"
+)
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class AuthenticationServiceApplication {
 
   public static void main(String[] args) {
