92b2b9e4616b0f47404c563d5abd010bcf2a036c
misharyduk <misharyduk64@gmail.com>
2023-02-10 14:51:12 +0200

    add json property annotations for fields

--- clients-common/src/main/java/com/publishing/clients/auth/RegisterRequest.java
+++ clients-common/src/main/java/com/publishing/clients/auth/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.publishing.clients.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.publishing.clients.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,9 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest extends UserRequest{
+    @JsonProperty("firstName")
     private String firstName;
+    @JsonProperty("lastName")
     private String lastName;
+    @JsonProperty("email")
     private String email;
+    @JsonProperty("password")
     private String password;
+    @JsonProperty("role")
     private Role role;
 }
