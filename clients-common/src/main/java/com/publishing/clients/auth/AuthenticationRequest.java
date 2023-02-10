f927a154da1afab0fb7035100e735e402f9783ea
misharyduk <misharyduk64@gmail.com>
2023-02-10 15:11:07 +0200

    remove Jackson annotations

--- clients-common/src/main/java/com/publishing/clients/auth/AuthenticationRequest.java
+++ clients-common/src/main/java/com/publishing/clients/auth/AuthenticationRequest.java
@@ -1,6 +1,5 @@
 package com.publishing.clients.auth;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthenticationRequest extends UserRequest{
-    @JsonProperty("email")
     private String email;
-    @JsonProperty("password")
     private String password;
 }
--- clients-common/src/main/java/com/publishing/clients/auth/RegisterRequest.java
+++ clients-common/src/main/java/com/publishing/clients/auth/RegisterRequest.java
@@ -1,6 +1,5 @@
 package com.publishing.clients.auth;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.publishing.clients.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,14 +11,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest extends UserRequest{
-    @JsonProperty("firstName")
     private String firstName;
-    @JsonProperty("lastName")
     private String lastName;
-    @JsonProperty("email")
     private String email;
-    @JsonProperty("password")
     private String password;
-    @JsonProperty("role")
     private Role role;
 }
