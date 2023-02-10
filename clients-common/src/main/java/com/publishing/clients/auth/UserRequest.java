0729ec882e3648fcf6fdcd65b425d9444b0a32de
misharyduk <misharyduk64@gmail.com>
2023-02-10 15:11:07 +0200

    remove Jackson annotations

--- clients-common/src/main/java/com/publishing/clients/auth/UserRequest.java
+++ clients-common/src/main/java/com/publishing/clients/auth/UserRequest.java
@@ -1,6 +1,5 @@
 package com.publishing.clients.auth;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +9,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class UserRequest {
-    @JsonProperty("username")
     private String username;
 }
