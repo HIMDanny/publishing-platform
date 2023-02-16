16a0ccb103888f6ce7890a1a0fe32f1d399bdf8c
misharyduk <misharyduk64@gmail.com>
2023-02-16 10:22:57 +0200

    add author field with Many-to-one relation

---
+++ clients-common/src/main/java/com/publishing/clients/article/Article.java
@@ -0,0 +1,44 @@
+package com.publishing.clients.article;
+
+import com.publishing.clients.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Article {
+    @SequenceGenerator(
+            name = "article_sequence",
+            sequenceName = "article_sequence"
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "article_sequence"
+    )
+    private Integer id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+    @Column
+    private String mainImagePath;
+    @Column
+    private LocalDateTime publishingDate;
+    @Column
+    private Integer minutesToRead;
+    @ManyToOne
+    private User author;
+//private Category category;
+
+    @Column
+    private Integer numberOfViews;
+    @Column
+    private Integer numberOfLikes;
+}
