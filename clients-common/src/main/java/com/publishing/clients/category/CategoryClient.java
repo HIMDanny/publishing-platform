1494341d511b10281e0573621466d09ccd9a8187
misharyduk <misharyduk64@gmail.com>
2023-02-16 14:00:12 +0200

    add open feign client for category with method to fetch category by id

---
+++ clients-common/src/main/java/com/publishing/clients/category/CategoryClient.java
@@ -0,0 +1,16 @@
+package com.publishing.clients.category;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "category-service",
+        path = "localhost:8087"
+)
+public interface CategoryClient {
+
+    @GetMapping("/dev/api/v1/categories/{categoryId}")
+    CategoryResponse getCategoryResponse(@PathVariable("categoryId") Integer categoryId);
+
+}
