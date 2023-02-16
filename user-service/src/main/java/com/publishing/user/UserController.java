3d185b0ee759471cd5d6e1e8fe1742898ab29d8e
misharyduk <misharyduk64@gmail.com>
2023-02-16 17:59:58 +0200

    add endpoint for fetching all users

--- user-service/src/main/java/com/publishing/user/UserController.java
+++ user-service/src/main/java/com/publishing/user/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
@@ -28,6 +30,11 @@ public class UserController {
   @GetMapping
   public User getByEmail(@RequestParam("email") String email){
     return userService.getUserByEmail(email);
+  }
+
+  @GetMapping
+  public List<User> getAllUsers(){
+    return userService.getAllUsers();
   }
 
   @PostMapping
