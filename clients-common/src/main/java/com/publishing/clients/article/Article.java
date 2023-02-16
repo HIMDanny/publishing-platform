22cb593fe932a3fc44dc107882a47b3f5ade80a9
misharyduk <misharyduk64@gmail.com>
2023-02-16 13:57:28 +0200

    change db relationship between article, category and user

--- clients-common/src/main/java/com/publishing/clients/article/Article.java
+++ clients-common/src/main/java/com/publishing/clients/article/Article.java
@@ -1,8 +1,10 @@
 package com.publishing.clients.article;
 
+import com.publishing.clients.category.Category;
 import com.publishing.clients.user.User;
 import jakarta.persistence.*;
 import lombok.*;
+import net.minidev.json.annotate.JsonIgnore;
 
 import java.time.LocalDateTime;
 
@@ -34,12 +36,18 @@ public class Article {
     @Column
     private Integer minutesToRead;
     @Column
+    @JsonIgnore
     private Integer authorId;
-//    @Column
-//    private Category category;
-
+    @Column
+    @JsonIgnore
+    private Integer categoryId;
     @Column
     private Integer numberOfViews;
     @Column
     private Integer numberOfLikes;
+
+    @Transient
+    private User user;
+    @Transient
+    private Category category;
 }
