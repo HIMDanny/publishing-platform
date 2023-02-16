f381c4c6d8120a094505c64be12089ea0447bc07
misharyduk <misharyduk64@gmail.com>
2023-02-16 10:23:31 +0200

    enable open feign client for article microservice

--- article-service/src/main/java/com/publishing/ArticleApplication.java
+++ article-service/src/main/java/com/publishing/ArticleApplication.java
@@ -1,11 +1,16 @@
 package com.publishing;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableFeignClients(
+        basePackages = "com.publishing.clients"
+)
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class ArticleApplication {
 
   public static void main(String[] args) {
