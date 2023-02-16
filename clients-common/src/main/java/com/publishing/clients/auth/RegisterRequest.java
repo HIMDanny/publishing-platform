ea6bf3fdf676861a22c590c49511d70ada6ec1f9
misharyduk <misharyduk64@gmail.com>
2023-02-16 09:53:42 +0200

    switch from @Data to @Getter and @Setter

--- clients-common/src/main/java/com/publishing/clients/auth/RegisterRequest.java
+++ clients-common/src/main/java/com/publishing/clients/auth/RegisterRequest.java
@@ -1,19 +1,23 @@
 package com.publishing.clients.auth;
 
 import com.publishing.clients.user.Role;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest extends UserRequest{
+//    @JsonProperty("firstName")
     private String firstName;
+//    @JsonProperty("lastName")
     private String lastName;
+//    @JsonProperty("email")
     private String email;
+//    @JsonProperty("password")
     private String password;
+//    @JsonProperty("role")
     private String role;
+
 }
