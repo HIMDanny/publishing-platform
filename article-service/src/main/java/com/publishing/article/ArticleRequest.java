c340db4173d910ae40b255617acbea448551636a
misharyduk <misharyduk64@gmail.com>
2023-02-16 15:03:25 +0200

    add category id to article request

--- article-service/src/main/java/com/publishing/article/ArticleRequest.java
+++ article-service/src/main/java/com/publishing/article/ArticleRequest.java
@@ -3,6 +3,7 @@ package com.publishing.article;
 public record ArticleRequest(String title,
                              String content,
                              String mainImagePath,
-                             Integer userId) {
+                             Integer userId,
+                             Integer categoryId) {
 
 }
