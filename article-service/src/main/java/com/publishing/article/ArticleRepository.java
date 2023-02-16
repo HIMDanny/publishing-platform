8c214bd3d9a366db864f162df80ee548c035d5dc
misharyduk <misharyduk64@gmail.com>
2023-02-16 13:58:59 +0200

    add repo method for fetching articles by category's id

--- article-service/src/main/java/com/publishing/article/ArticleRepository.java
+++ article-service/src/main/java/com/publishing/article/ArticleRepository.java
@@ -3,6 +3,10 @@ package com.publishing.article;
 import com.publishing.clients.article.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
+
+    List<Article> findAllByCategoryId(Integer categoryId);
 
 }
