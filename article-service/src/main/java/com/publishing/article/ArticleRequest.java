721846b9538feb45d56d2df00e561c795b76433f
misharyduk <misharyduk64@gmail.com>
2023-02-18 21:23:32 +0200

    remove unused classes

--- article-service/src/main/java/com/publishing/article/ArticleRequest.java
+++
@@ -1,9 +0,0 @@
-package com.publishing.article;
-
-public record ArticleRequest(String title,
-                             String content,
-                             String mainImagePath,
-                             Integer authorId,
-                             Integer categoryId) {
-
-}
