55fad6cb7b8a2ed7434e930b185517f85c84aecf
misharyduk <misharyduk64@gmail.com>
2023-02-18 22:22:05 +0200

    add dto class for request

---
+++ clients-common/src/main/java/com/publishing/clients/user/dto/UserRequestDto.java
@@ -0,0 +1,15 @@
+package com.publishing.clients.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
