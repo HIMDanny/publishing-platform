ac56d65bdf40b1bf5984a5d72e59a075d84c9db0
misharyduk <misharyduk64@gmail.com>
2023-02-18 17:49:26 +0200

    add response http status

--- article-service/src/main/java/com/publishing/article/ArticleController.java
+++ article-service/src/main/java/com/publishing/article/ArticleController.java
@@ -4,14 +4,8 @@ import com.publishing.clients.article.Article;
 import com.publishing.exception.ArticleException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -21,22 +15,26 @@ public class ArticleController {
   private final ArticleService articleService;
 
   @GetMapping("{id}")
+  @ResponseStatus(HttpStatus.OK)
   public Article getArticleById(@PathVariable("id") Integer id) throws ArticleException {
     // TODO: handle exception
     return articleService.getArticle(id);
   }
 
   @GetMapping
+  @ResponseStatus(HttpStatus.OK)
   public List<Article> getAllArticles(){
     return articleService.getAllArticles();
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public Article saveArticle(@RequestBody ArticleRequest articleRequest){
     return articleService.saveArticle(articleRequest);
   }
 
   @PutMapping("{id}")
+  @ResponseStatus(HttpStatus.OK)
   public Article updateArticle(@PathVariable("id") Integer id, @RequestBody ArticleRequest articleRequest)
       throws ArticleException {
     // TODO: handle exception
@@ -45,6 +43,7 @@ public class ArticleController {
   }
 
   @DeleteMapping("{id}")
+  @ResponseStatus(HttpStatus.OK)
   public void deleteArticle(@PathVariable("id") Integer id) throws ArticleException {
     // TODO: handle exception
 
