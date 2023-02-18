d6f501e48a39884a2e9aa2cebd298d22a4ec0c75
misharyduk <misharyduk64@gmail.com>
2023-02-18 18:16:01 +0200

    add response http status

--- authentication-service/src/main/java/com/publishing/auth/AuthenticationController.java
+++ authentication-service/src/main/java/com/publishing/auth/AuthenticationController.java
@@ -3,11 +3,9 @@ package com.publishing.auth;
 import com.publishing.clients.auth.RegisterRequest;
 import com.publishing.clients.auth.AuthenticationRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -17,6 +15,7 @@ public class AuthenticationController {
   private final AuthenticationService service;
 
   @PostMapping("register")
+  @ResponseStatus(HttpStatus.CREATED)
   public ResponseEntity<AuthenticationResponse> register(
       @RequestBody RegisterRequest request
   ){
@@ -24,6 +23,7 @@ public class AuthenticationController {
   }
 
   @PostMapping("authenticate")
+  @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<AuthenticationResponse> authenticate(
     @RequestBody AuthenticationRequest request
   ){
