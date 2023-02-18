59b6f7fa60573e5a80095b34804ae345cab56781
misharyduk <misharyduk64@gmail.com>
2023-02-18 22:19:12 +0200

    change import for user

--- article-service/src/main/java/com/publishing/article/repo/ArticleRepository.java
+++ article-service/src/main/java/com/publishing/article/repo/ArticleRepository.java
@@ -1,6 +1,6 @@
-package com.publishing.article;
+package com.publishing.article.repo;
 
-import com.publishing.clients.article.Article;
+import com.publishing.article.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
