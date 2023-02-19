6133b5675312c30ded0aad831ca93510a09c69f6
misharyduk <misharyduk64@gmail.com>
2023-02-19 10:18:36 +0200

    add http response status

--- clients-common/src/main/java/com/publishing/clients/category/CategoryClient.java
+++ clients-common/src/main/java/com/publishing/clients/category/CategoryClient.java
@@ -2,8 +2,10 @@ package com.publishing.clients.category;
 
 import com.publishing.clients.category.dto.CategoryResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @FeignClient(
         name = "category-service",
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface CategoryClient {
 
     @GetMapping("dev/api/v1/categories/{categoryId}")
+    @ResponseStatus(HttpStatus.OK)
     CategoryResponseDto getCategoryResponse(@PathVariable("categoryId") Integer categoryId);
 
 }
