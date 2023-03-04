1198be980d23b8392b463edc423df48190c60c16
HIMDanny <danilbliz0201@gmail.com>
2023-03-04 17:25:24 +0200

    feat: change default font to Montserrat

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require('prettier-plugin-tailwindcss')],
 };
