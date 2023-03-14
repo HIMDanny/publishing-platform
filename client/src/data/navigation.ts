c4bf41d41165d14d3d8aca62c9d58bd6c1443f80
HIMDanny <danilbliz0201@gmail.com>
2023-03-14 22:34:41 +0200

    feat: create navigation link constants
    Made to easier maintain navigation links across application

---
+++ client/src/data/navigation.ts
@@ -0,0 +1,16 @@
+const navigation = [
+  {
+    label: 'Home',
+    to: '/',
+  },
+  {
+    label: 'Category',
+    to: '/categories',
+  },
+  {
+    label: 'Write',
+    to: '/write',
+  },
+] as const;
+
+export default navigation;
