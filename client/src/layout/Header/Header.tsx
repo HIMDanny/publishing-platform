613b554c7e8e590f07360186e09c26c65d1593e7
HIMDanny <danilbliz0201@gmail.com>
2023-03-18 18:59:30 +0200

    feat: position header to absolute

--- client/src/layout/Header/Header.tsx
+++ client/src/layout/Header/Header.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
-import Logo from '@components/Logo';
+import Logo from '@components/UI/Logo';
 import HeaderActions from './HeaderActions';
 import HeaderNavigation from './HeaderNavigation';
 
 const Header = () => {
   return (
-    <header className="py-6">
+    <header className="absolute top-0 w-full py-6">
       <div className="container mx-auto grid grid-cols-3 items-center">
         <Link
           to="/"
