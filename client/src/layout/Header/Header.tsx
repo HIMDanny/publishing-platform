6eed5b46f4b5a2dc85664f2b1f0448043e5c5a38
HIMDanny <danilbliz0201@gmail.com>
2023-03-18 11:42:19 +0200

    feat: chagne default link to react router component

--- client/src/layout/Header/Header.tsx
+++ client/src/layout/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Logo from '@components/Logo';
 import HeaderActions from './HeaderActions';
 import HeaderNavigation from './HeaderNavigation';
@@ -6,9 +7,12 @@ const Header = () => {
   return (
     <header className="py-6">
       <div className="container mx-auto grid grid-cols-3 items-center">
-        <a href="/">
+        <Link
+          to="/"
+          className="w-fit"
+        >
           <Logo />
-        </a>
+        </Link>
 
         <HeaderNavigation />
 
