2edb6747550ca900f66837ac579c72de5680597e
HIMDanny <danilbliz0201@gmail.com>
2023-03-24 19:20:41 +0200

    feat: create post card section layout

---
+++ client/src/features/posts/components/PostCardSection/PostCardSection.tsx
@@ -0,0 +1,11 @@
+import PostCard from '../PostCard/PostCard';
+
+const PostCardSection = () => {
+  return (
+    <section>
+      <h4>Trending</h4>
+      <PostCard />
+    </section>
+  );
+};
+export default PostCardSection;
