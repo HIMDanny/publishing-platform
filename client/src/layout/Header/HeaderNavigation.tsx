560696983f9bf2424967c0598fb092cd4208f566
HIMDanny <danilbliz0201@gmail.com>
2023-03-25 12:37:40 +0200

    chore: delete unnecessary classname on li item

--- client/src/layout/Header/HeaderNavigation.tsx
+++ client/src/layout/Header/HeaderNavigation.tsx
@@ -6,10 +6,7 @@ const HeaderNavigation = () => {
     <nav className="w-fit justify-self-center rounded-full border-2 border-gray-50 py-[0.875rem] px-[3.75rem]">
       <ul className="flex gap-10 text-gray-50">
         {navigation.map((link) => (
-          <li
-            key={link.label}
-            className="relative"
-          >
+          <li key={link.label}>
             <NavigationLink
               label={link.label}
               to={link.to}
