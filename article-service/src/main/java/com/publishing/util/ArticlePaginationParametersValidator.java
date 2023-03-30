2673f653ffea8742db56cd354204fcc18c5148fe
misharyduk <misharyduk64@gmail.com>
2023-03-30 19:47:22 +0300

    implement article pagination parameters validation

---
+++ article-service/src/main/java/com/publishing/util/ArticlePaginationParametersValidator.java
@@ -0,0 +1,48 @@
+package com.publishing.util;
+
+import com.google.common.collect.Lists;
+import com.publishing.clients.ICheckPaginationParameters;
+import org.springframework.stereotype.Component;
+
+import java.util.EnumSet;
+import java.util.List;
+
+@Component
+public class ArticlePaginationParametersValidator implements ICheckPaginationParameters {
+
+    enum ArticleFields{
+
+        ID(Lists.newArrayList("id", "ID")),
+        TITLE(Lists.newArrayList("title", "TITLE", "name")),
+        PUBLISHING_DATE(Lists.newArrayList("publishing_date", "date", "publishingDate",
+                "PUBLISHING_DATE", "PUBLISHINGDATE", "DATE")),
+        NUMBER_OF_VIEWS(Lists.newArrayList("number_of_views", "number_of_view", "views", "view", "numberOfViews",
+                "NUMBER_OF_VIEWS", "NUMBER_OF_VIEW", "NUMBEROFVIEWS", "VIEWS", "VIEWS")),
+        NUMBER_OF_LIKES(Lists.newArrayList("number_of_likes", "number_of_like", "likes", "like", "numberOfLikes",
+                "NUMBER_OF_LIKES", "NUMBER_OF_LIKE", "NUMBEROFLIKES", "LIKES", "LIKE"));
+
+        private final List<String> fieldValues;
+
+
+        ArticleFields(List<String> fieldValues) {
+            this.fieldValues = fieldValues;
+        }
+
+        public List<String> getFieldValues() {
+            return fieldValues;
+        }
+    }
+
+    @Override
+    public boolean isCorrect(String field) {
+        EnumSet<ArticleFields> articleFieldsSet = EnumSet.allOf(ArticleFields.class);
+        boolean isCorrect = articleFieldsSet.contains(ArticleFields.valueOf(field.toUpperCase()));
+        if(!isCorrect){
+            for (ArticleFields articleFields : articleFieldsSet) {
+                boolean isContains = articleFields.getFieldValues().contains(field);
+                if (isContains) return true;
+            }
+        }
+        return isCorrect;
+    }
+}
