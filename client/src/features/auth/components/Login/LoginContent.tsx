0f604e24c6782b120091df4bb84580883cd1e686
HIMDanny <danilbliz0201@gmail.com>
2023-03-31 23:11:56 +0300

    feat: create login part content

---
+++ client/src/features/auth/components/Login/LoginContent.tsx
@@ -0,0 +1,23 @@
+import LoginForm from './LoginForm';
+import BackButton from '../BackButton';
+import SideContent from '../SideContent';
+
+const LoginContent = () => {
+  return (
+    <>
+      <div className="px-16 pt-20 pb-8">
+        <LoginForm />
+        <div className="mt-9">
+          <BackButton />
+        </div>
+      </div>
+      <SideContent
+        title="Welcome Back!"
+        body="In case you are not signed up, click on the button below to start your
+          journey with us"
+        buttonText="Sign Up"
+      />
+    </>
+  );
+};
+export default LoginContent;
