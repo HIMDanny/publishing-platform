f5fd1f327538f4c7635bcd4e3467840413a91225
HIMDanny <danilbliz0201@gmail.com>
2023-03-31 23:12:17 +0300

    feat: create login form layout

---
+++ client/src/features/auth/components/Login/LoginForm.tsx
@@ -0,0 +1,36 @@
+import Button from '@components/UI/Button';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h3>Log In</h3>
+      <div className="my-8">Icons</div>
+      <form className="w-full max-w-sm text-center">
+        <div className="flex flex-col gap-4">
+          <input
+            className="rounded-md border"
+            type="email"
+          />
+          <input
+            className="rounded-md border"
+            type="text"
+          />
+        </div>
+        <Link
+          to="#"
+          className="mt-7 mb-6 block"
+        >
+          Forgot your password?
+        </Link>
+        <Button
+          size="lg"
+          variant="secondary"
+        >
+          Sign In
+        </Button>
+      </form>
+    </div>
+  );
+};
+export default LoginForm;
