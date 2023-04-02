754498bd5c59aef55dce591a03a57dac82190e47
misharyduk <misharyduk64@gmail.com>
2023-04-02 19:28:15 +0300

    add discovery service settings

--- article-service/src/main/java/com/publishing/ArticleApplication.java
+++ article-service/src/main/java/com/publishing/ArticleApplication.java
@@ -3,12 +3,10 @@ package com.publishing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.publishing.clients"
 )
