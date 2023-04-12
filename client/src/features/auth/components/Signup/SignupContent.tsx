4d1a5b5d0a0506fb3b4407684c9a37447a57ab80
HIMDanny <danilbliz0201@gmail.com>
2023-04-12 13:14:40 +0300

    feat: create signup content layout

--- client/src/features/auth/components/Signup/SignupContent.tsx
+++ client/src/features/auth/components/Signup/SignupContent.tsx
@@ -1,4 +1,23 @@
+import BackButton from '../BackButton';
+import SideContent from '../SideContent';
+import SignupForm from './SignupForm';
+
 const SignupContent = () => {
-  return <div>SignupContent</div>;
+  return (
+    <>
+      <SideContent
+        title="Hello, Friends!"
+        body="To keep connected whith us plesase login with your personal info"
+        buttonText="Sign In"
+        navigateTo="?mode=login"
+      />
+      <div className="px-16 pt-20 pb-8">
+        <SignupForm />
+        <div className="mt-9 flex justify-end">
+          <BackButton />
+        </div>
+      </div>
+    </>
+  );
 };
 export default SignupContent;
