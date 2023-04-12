edf872a8f2dab55a30bc12fa8f28ce8b53e612f2
HIMDanny <danilbliz0201@gmail.com>
2023-04-12 14:48:25 +0300

    feat: create universal Field component

---
+++ client/src/components/Field/Field.tsx
@@ -0,0 +1,11 @@
+import TextField from './TextField';
+
+const Field = () => {
+  return (
+    <TextField
+      label="Name"
+      labelProps={{ className: 'bg-gray-50' }}
+    />
+  );
+};
+export default Field;
