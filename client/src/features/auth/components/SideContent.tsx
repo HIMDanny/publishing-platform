0f663514cffd28ec6d83c1965915df33097e8550
HIMDanny <danilbliz0201@gmail.com>
2023-04-12 13:59:33 +0300

    fix: change stylings

--- client/src/features/auth/components/SideContent.tsx
+++ client/src/features/auth/components/SideContent.tsx
@@ -15,9 +15,9 @@ const SideContent: React.FC<SideContentProps> = ({
   navigateTo,
 }) => {
   return (
-    <div className="isolate space-y-10 px-16 pt-20 pb-8 text-center text-gray-50">
+    <div className="isolate px-16 pt-20 pb-8 text-center text-gray-100">
       <h3>{title}</h3>
-      <p>{body}</p>
+      <p className="mt-9 mb-10 text-gray-100 text-opacity-70">{body}</p>
       <Button
         size="lg"
         text={buttonText}
