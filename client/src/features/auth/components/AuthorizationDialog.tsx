fee6722c48dd032673467438d003106d9f64d0dc
HIMDanny <danilbliz0201@gmail.com>
2023-04-12 13:15:33 +0300

    feat: add signup content & add transition

--- client/src/features/auth/components/AuthorizationDialog.tsx
+++ client/src/features/auth/components/AuthorizationDialog.tsx
@@ -1,10 +1,26 @@
+import { useSearchParams } from 'react-router-dom';
 import LoginContent from './Login/LoginContent';
-
+import SignupContent from './Signup/SignupContent';
+import classNames from 'classnames';
+import { AnimatePresence } from 'framer-motion';
 const AuthorizationDialog = () => {
+  const [searchParams] = useSearchParams();
+
+  const isLogin = searchParams.get('mode') === 'login';
+
   return (
     <div className="relative mt-20 grid w-full max-w-[1200px] grid-cols-2 items-center overflow-hidden rounded-[40px] bg-gray-50">
-      <div className="absolute right-0 h-full w-1/2 bg-green-700" />
-      <LoginContent />
+      <div
+        className={classNames(
+          'absolute left-0 h-full w-1/2 bg-green-700 transition-all duration-500',
+          {
+            'translate-x-full': isLogin,
+          },
+        )}
+      />
+      <AnimatePresence>
+        {isLogin ? <LoginContent /> : <SignupContent />}
+      </AnimatePresence>
     </div>
   );
 };
