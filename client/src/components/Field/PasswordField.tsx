6e41f5a627ec1fdb5de7f4bcb23424088a2b91d3
HIMDanny <danilbliz0201@gmail.com>
2023-04-13 21:31:43 +0300

    feat: add error state in fields

--- client/src/components/Field/PasswordField.tsx
+++ client/src/components/Field/PasswordField.tsx
@@ -26,7 +26,12 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
           type={inputType}
           id={field.name}
           className={classNames(
-            'peer block w-full appearance-none rounded-md border border-gray-900 bg-transparent px-3 py-4 text-sm text-gray-900 placeholder-shown:border-gray-500 hover:border-gray-400 focus:border-gray-900 focus:outline-none focus:ring-0',
+            'peer block w-full appearance-none rounded-md border bg-transparent px-3 py-4 text-sm focus:outline-none focus:ring-0',
+            {
+              'border-gray-900 text-gray-900 placeholder-shown:border-gray-500 hover:border-gray-400 focus:border-gray-900':
+                !isError,
+            },
+            { 'border-error text-error hover:border-opacity-50': isError },
           )}
           placeholder=" "
           {...field}
@@ -35,7 +40,9 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
         <label
           htmlFor={field.name}
           className={classNames(
-            'pointer-events-none absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform px-2 text-gray-900 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-gray-400 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2',
+            'pointer-events-none absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform px-2  duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100  peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2',
+            { 'text-gray-900 peer-placeholder-shown:text-gray-400': !isError },
+            { 'text-error': isError },
             labelProps?.className ?? '',
           )}
         >
@@ -47,7 +54,11 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
         />
       </div>
       {isError && (
-        <p className={classNames('pr-2 text-right text-sm text-gray-600')}>
+        <p
+          className={classNames('pr-2 text-right text-sm text-gray-600', {
+            'text-error': isError,
+          })}
+        >
           {meta.error}
         </p>
       )}
