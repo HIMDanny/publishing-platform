255bf62de785386b4059a94569f438e379342414
HIMDanny <danilbliz0201@gmail.com>
2023-04-13 19:40:19 +0300

    fix: add fixed height to content wrapper

--- client/src/features/auth/components/AuthorizationDialog.tsx
+++ client/src/features/auth/components/AuthorizationDialog.tsx
@@ -8,7 +8,7 @@ const AuthorizationDialog = () => {
   const isLogin = searchParams.get('mode') === 'login';
 
   return (
-    <div className="relative mt-20 grid w-full max-w-[1200px] grid-cols-2 items-center overflow-hidden rounded-[40px] bg-gray-50">
+    <div className="relative mt-20 grid h-[590px] w-full max-w-[1200px] grid-cols-2 items-center overflow-hidden rounded-[40px] bg-gray-50">
       <div
         className={classNames(
           'absolute left-0 h-full w-1/2 bg-green-700 transition-all duration-500',
