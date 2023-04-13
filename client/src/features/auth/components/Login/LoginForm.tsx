62ac9b13bd4826d34db409b8356dd4d0ac250d34
HIMDanny <danilbliz0201@gmail.com>
2023-04-13 17:53:11 +0300

    feat: change default form to Formik

--- client/src/features/auth/components/Login/LoginForm.tsx
+++ client/src/features/auth/components/Login/LoginForm.tsx
@@ -1,32 +1,50 @@
 import Field from '@components/Field/Field';
 import Button from '@components/UI/Button';
 import { Link } from 'react-router-dom';
+import { Formik, Form } from 'formik';
 
 const LoginForm = () => {
   return (
     <div className="flex flex-col items-center">
       <h3>Log In</h3>
       <div className="my-8">Icons</div>
-      <form className="w-full max-w-sm text-center">
-        <div className="flex flex-col gap-4">
-          <Field />
-          <input
-            className="rounded-md border"
-            type="text"
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={() => {
+          return;
+        }}
+      >
+        <Form className="w-full max-w-sm text-center">
+          <div className="flex flex-col gap-4">
+            <Field
+              type="email"
+              name="email"
+              label="Email"
+              labelProps={{ className: 'bg-gray-50' }}
+            />
+            <Field
+              type="password"
+              name="password"
+              label="Password"
+              labelProps={{ className: 'bg-gray-50' }}
+            />
+          </div>
+          <Link
+            to="#"
+            className="mt-7 mb-6 block"
+          >
+            Forgot your password?
+          </Link>
+          <Button
+            size="lg"
+            variant="secondary"
+            text="Sign In"
           />
-        </div>
-        <Link
-          to="#"
-          className="mt-7 mb-6 block"
-        >
-          Forgot your password?
-        </Link>
-        <Button
-          size="lg"
-          variant="secondary"
-          text="Sign In"
-        />
-      </form>
+        </Form>
+      </Formik>
     </div>
   );
 };
