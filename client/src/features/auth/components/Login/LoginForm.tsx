a49d0c35eaac866765ff5a36d8179c064bd8135f
HIMDanny <danilbliz0201@gmail.com>
2023-04-13 21:30:32 +0300

    feat: disable button if form is not valid

--- client/src/features/auth/components/Login/LoginForm.tsx
+++ client/src/features/auth/components/Login/LoginForm.tsx
@@ -3,6 +3,7 @@ import Button from '@components/UI/Button';
 import { Link } from 'react-router-dom';
 import { Formik, Form } from 'formik';
 import AuthIcons from '../AuthIcons';
+import { SigninSchema } from '@features/auth/utils/validation-schemas';
 
 const LoginForm = () => {
   return (
@@ -10,6 +11,7 @@ const LoginForm = () => {
       <h3>Log In</h3>
       <AuthIcons />
       <Formik
+        validationSchema={SigninSchema}
         initialValues={{
           email: '',
           password: '',
@@ -18,33 +20,36 @@ const LoginForm = () => {
           return;
         }}
       >
-        <Form className="w-full max-w-sm text-center">
-          <div className="flex flex-col gap-4">
-            <Field
-              type="email"
-              name="email"
-              label="Email"
-              labelProps={{ className: 'bg-gray-50' }}
+        {({ isValid }) => (
+          <Form className="w-full max-w-sm text-center">
+            <div className="flex flex-col gap-4">
+              <Field
+                type="email"
+                name="email"
+                label="Email"
+                labelProps={{ className: 'bg-gray-50' }}
+              />
+              <Field
+                type="password"
+                name="password"
+                label="Password"
+                labelProps={{ className: 'bg-gray-50' }}
+              />
+            </div>
+            <Link
+              to="#"
+              className="mt-7 mb-6 block"
+            >
+              Forgot your password?
+            </Link>
+            <Button
+              disabled={!isValid}
+              size="lg"
+              variant="secondary"
+              text="Sign In"
             />
-            <Field
-              type="password"
-              name="password"
-              label="Password"
-              labelProps={{ className: 'bg-gray-50' }}
-            />
-          </div>
-          <Link
-            to="#"
-            className="mt-7 mb-6 block"
-          >
-            Forgot your password?
-          </Link>
-          <Button
-            size="lg"
-            variant="secondary"
-            text="Sign In"
-          />
-        </Form>
+          </Form>
+        )}
       </Formik>
     </div>
   );
--- client/src/features/auth/components/Signup/SignupForm.tsx
+++ client/src/features/auth/components/Signup/SignupForm.tsx
@@ -2,6 +2,7 @@ import Field from '@components/Field/Field';
 import Button from '@components/UI/Button';
 import { Formik, Form } from 'formik';
 import AuthIcons from '../AuthIcons';
+import { SignupSchema } from '@features/auth/utils/validation-schemas';
 
 const SignupForm = () => {
   return (
@@ -9,6 +10,7 @@ const SignupForm = () => {
       <h3>Create Acccount</h3>
       <AuthIcons />
       <Formik
+        validationSchema={SignupSchema}
         initialValues={{
           name: '',
           email: '',
@@ -18,33 +20,36 @@ const SignupForm = () => {
           return;
         }}
       >
-        <Form className="w-full max-w-sm text-center">
-          <div className="mb-6 flex flex-col gap-4">
-            <Field
-              type="text"
-              label="Name"
-              name="name"
-              labelProps={{ className: 'bg-gray-50' }}
+        {({ isValid }) => (
+          <Form className="w-full max-w-sm text-center">
+            <div className="mb-6 flex flex-col gap-4">
+              <Field
+                type="text"
+                label="Name"
+                name="name"
+                labelProps={{ className: 'bg-gray-50' }}
+              />
+              <Field
+                type="email"
+                label="Email"
+                name="email"
+                labelProps={{ className: 'bg-gray-50' }}
+              />
+              <Field
+                type="password"
+                label="Password"
+                name="password"
+                labelProps={{ className: 'bg-gray-50' }}
+              />
+            </div>
+            <Button
+              disabled={!isValid}
+              size="lg"
+              variant="secondary"
+              text="Sign In"
             />
-            <Field
-              type="email"
-              label="Email"
-              name="email"
-              labelProps={{ className: 'bg-gray-50' }}
-            />
-            <Field
-              type="password"
-              label="Password"
-              name="password"
-              labelProps={{ className: 'bg-gray-50' }}
-            />
-          </div>
-          <Button
-            size="lg"
-            variant="secondary"
-            text="Sign In"
-          />
-        </Form>
+          </Form>
+        )}
       </Formik>
     </div>
   );
