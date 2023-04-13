17040b2200f0f260e64e583f35c6f1a6e958863c
HIMDanny <danilbliz0201@gmail.com>
2023-04-13 19:08:37 +0300

    feat: add formik fields to signup form

--- client/src/features/auth/components/Signup/SignupForm.tsx
+++ client/src/features/auth/components/Signup/SignupForm.tsx
@@ -1,31 +1,50 @@
+import Field from '@components/Field/Field';
 import Button from '@components/UI/Button';
+import { Formik, Form } from 'formik';
 
 const SignupForm = () => {
   return (
     <div className="flex flex-col items-center">
       <h3>Create Acccount</h3>
       <div className="my-8">Icons</div>
-      <form className="w-full max-w-sm text-center">
-        <div className="mb-6 flex flex-col gap-4">
-          <input
-            className="rounded-md border"
-            type="email"
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          password: '',
+        }}
+        onSubmit={() => {
+          return;
+        }}
+      >
+        <Form className="w-full max-w-sm text-center">
+          <div className="mb-6 flex flex-col gap-4">
+            <Field
+              type="text"
+              label="Name"
+              name="name"
+              labelProps={{ className: 'bg-gray-50' }}
+            />
+            <Field
+              type="email"
+              label="Email"
+              name="email"
+              labelProps={{ className: 'bg-gray-50' }}
+            />
+            <Field
+              type="password"
+              label="Password"
+              name="password"
+              labelProps={{ className: 'bg-gray-50' }}
+            />
+          </div>
+          <Button
+            size="lg"
+            variant="secondary"
+            text="Sign In"
           />
-          <input
-            className="rounded-md border"
-            type="text"
-          />
-          <input
-            className="rounded-md border"
-            type="text"
-          />
-        </div>
-        <Button
-          size="lg"
-          variant="secondary"
-          text="Sign In"
-        />
-      </form>
+        </Form>
+      </Formik>
     </div>
   );
 };
