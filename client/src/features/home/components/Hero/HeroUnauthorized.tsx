aff1b685db786513352194906dd8aa83c3434df3
HIMDanny <danilbliz0201@gmail.com>
2023-05-21 17:40:20 +0300

    fix: make several fixes
    - Change bg size to cover
    - Change display of section to flex

--- client/src/features/home/components/Hero/HeroUnauthorized.tsx
+++ client/src/features/home/components/Hero/HeroUnauthorized.tsx
@@ -5,11 +5,11 @@ const Hero = () => {
   return (
     <section
       className={classNames(
-        'flow-root h-screen bg-gray-800',
-        `bg-[url(./hero-section-unauthorized-bg.png)] bg-center bg-no-repeat`,
+        'flex h-screen items-center bg-gray-800',
+        `bg-[url(./hero-section-unauthorized-bg.png)] bg-cover bg-center bg-no-repeat`,
       )}
     >
-      <div className="container mx-auto mt-72">
+      <div className="container mx-auto">
         <h2 className="text-8xl text-white">Find your topic!</h2>
         <p className="mt-9 mb-10 max-w-2xl text-2xl text-gray-50 text-opacity-70">
           Praesent id volutpat sapien, vitae sollicitudin lorem. Integer non
