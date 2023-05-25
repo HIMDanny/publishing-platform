c1a5e8e724f6d8c84a1863d9c81cef2662b8cce2
HIMDanny <danilbliz0201@gmail.com>
2023-05-25 15:21:11 +0300

    feat: create hero layout

--- client/src/features/home/components/Authorized/Hero.tsx
+++ client/src/features/home/components/Authorized/Hero.tsx
@@ -1,4 +1,15 @@
+import classNames from 'classnames';
+
 const Hero = () => {
-  return <section></section>;
+  return (
+    <section
+      className={classNames(
+        'pb-36 pt-60 text-white',
+        'bg-[url(./small-bg-with-lights.png)] bg-cover bg-center bg-no-repeat',
+      )}
+    >
+      <h2 className="heading-1 text-center">Find your topic!</h2>
+    </section>
+  );
 };
 export default Hero;
